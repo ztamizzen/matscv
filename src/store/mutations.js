cc92742604575f88a3bf04269fca9614d91a6822
Mats Lindblad <mats.lindblad@gmail.com>
2019-10-29 22:09:07 +0100

    updated package-lock

---
+++ src/store/mutations.js
@@ -0,0 +1,20 @@
+// Vuex style reducers, called Mutations.
+export const STORAGE_KEY = 'todos-vuejs';
+export const types = {
+    ADD_TODO: 'addTodo',
+    EDIT_TODO: 'editTodo',
+    REMOVE_TODO: 'removeTodo'
+};
+export const mutations = {
+    [types.ADD_TODO]: (state, todo) => {
+        state.todos = [...state.todos, todo];
+    },
+    [types.EDIT_TODO]: (state, { todo, text = todo.text, done = todo.done }) => {
+        todo.text = text;
+        todo.done = done;
+    },
+    [types.REMOVE_TODO]: (state, todo) => {
+        const indexOfTodo = state.todos.indexOf(todo);
+        state.todos = [...state.todos.slice(0, indexOfTodo), ...state.todos.slice(indexOfTodo + 1)];
+    }
+};
